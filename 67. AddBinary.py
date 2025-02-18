779de0232c25022e665f3bf4024403de6cdbc182
Ella <michaella.latkovic@gmail.com>
2025-02-18 09:40:16 -0800

    style update

---
+++ 67. AddBinary.py
@@ -0,0 +1,24 @@
+"""
+LeetCode: 67. Add Binary
+Difficulty: Easy
+URL: https://leetcode.com/problems/add-binary/
+Tags: Math, String
+
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+"""
+
+from typing import List
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        pass 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# LeetCode
+### LeetCode Top Interview 150 Solutions
+Documenting my way through the LeetCode Top Interview 150 problems. This collection is mainly to serve as a reference. 
+
